21f723502c88d0d1d972d7e66abd3ff2dbb641f4
gleb619 <gleb86496@gmail.com>
2025-06-07 15:15:48 +0500

    flow changes v1

--- src/main/java/machinum/processor/core/FlowSupport.java
+++ src/main/java/machinum/processor/core/FlowSupport.java
@@ -15,9 +15,13 @@ import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.core.io.Resource;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+import static machinum.flow.FlowContextActions.alt;
+import static machinum.flow.FlowContextActions.glossary;
 import static machinum.util.JavaUtil.*;
 import static machinum.util.TextUtil.countHistoryTokens;
 import static machinum.util.TextUtil.countTokens;
@@ -89,92 +93,20 @@ public interface FlowSupport {
                 .build());
     }
 
-    default List<Message> fulfillHistory(HistoryContext historyContext) {
-        var items = List.of(historyContext.getAllowedItems().isEmpty() ? HistoryItem.values() : historyContext.getAllowedItems().toArray(HistoryItem[]::new));
-        var names = new ArrayList<ObjectName>();
-        var history = new ArrayList<Message>();
-        var previousContext = new StringBuilder();
-        history.add(new SystemMessage(historyContext.getSystemMessage()));
-        var systemTokens = countTokens(historyContext.getSystemMessage());
-        historyContext.getBudget().allocate(systemTokens);
+    //TODO redo StringBuilder to arraylist
+    private static void processContext(HistoryContext historyContext, FlowArgument<String> contextArg,
+                                       StringBuilder previousContext, List<Message> history, String templateText) {
+        var context = contextArg.stringValue();
+        int itemTokens = contextArg.countTokens();
 
-        if (items.contains(HistoryItem.CONSOLIDATED_CONTEXT)) {
-            historyContext.getFlowContext().hasAnyArgument(oldContext -> {
-                var context = oldContext.stringValue();
-                int itemTokens = countTokens(context);
+        if (historyContext.getBudget().canAllocate(itemTokens)) {
+            previousContext.append(context);
 
-                if (historyContext.getBudget().canAllocate(itemTokens)) {
-                    previousContext.append(context);
+            history.add(new UserMessage(templateText));
+            history.add(new AssistantMessage(context));
 
-                    history.add(new UserMessage(USER_PREVIOUS_CONTEXT_TEMPLATE));
-                    history.add(new AssistantMessage(context));
-
-                    historyContext.getBudget().allocate(itemTokens);
-                }
-            }, FlowContext::consolidatedContextArg, FlowContext::oldContexArg);
+            historyContext.getBudget().allocate(itemTokens);
         }
-
-        if (items.contains(HistoryItem.CONTEXT)) {
-            historyContext.getFlowContext().hasArgument(FlowContext::contextArg, context -> {
-                var currentContext = context.stringValue();
-
-                if (!previousContext.toString().equals(currentContext)) {
-                    int itemTokens = countTokens(currentContext);
-
-                    if (historyContext.getBudget().canAllocate(itemTokens)) {
-                        history.add(new UserMessage(USER_CONTEXT_TEMPLATE));
-                        history.add(new AssistantMessage(currentContext));
-
-                        historyContext.getBudget().allocate(itemTokens);
-                    }
-                }
-            });
-        }
-
-        if (items.contains(HistoryItem.CONSOLIDATED_GLOSSARY)) {
-            historyContext.getFlowContext().hasAnyArgument(oldGlossary -> {
-                int itemTokens = oldGlossary.countTokens();
-
-                if (historyContext.getBudget().canAllocate(itemTokens)) {
-                    history.add(new UserMessage(USER_PREVIOUS_GLOSSARY_TEMPLATE));
-                    var arg = oldGlossary.map(list -> uniqueBy(list, ObjectName::getName));
-
-                    if (arg.countWords() > 700) {
-                        history.add(new AssistantMessage(arg.shortStringValue()));
-                    } else {
-                        history.add(new AssistantMessage(arg.stringValue()));
-                    }
-
-                    names.addAll(oldGlossary.getValue());
-
-                    historyContext.getBudget().allocate(itemTokens);
-                }
-            }, FlowContext::consolidatedGlossaryArg, FlowContext::oldGlossaryArg);
-        }
-
-        if (items.contains(HistoryItem.GLOSSARY)) {
-            historyContext.getFlowContext().hasArgument(FlowContext::glossaryArg, glossary -> {
-                int itemTokens = glossary.countTokens();
-
-                if (historyContext.getBudget().canAllocate(itemTokens)) {
-                    var smallList = glossary.map(chapterNames -> uniqueItems(names, chapterNames, ObjectName::getName));
-
-                    if (!smallList.isEmpty()) {
-                        history.add(new UserMessage(USER_GLOSSARY_TEMPLATE));
-
-                        if (smallList.countWords() > 1_500) {
-                            history.add(new AssistantMessage(smallList.shortStringValue()));
-                        } else {
-                            history.add(new AssistantMessage(smallList.stringValue()));
-                        }
-
-                        historyContext.getBudget().allocate(itemTokens);
-                    }
-                }
-            });
-        }
-
-        return history;
     }
 
     default List<Message> fulfillShortHistory(Resource systemMessage, FlowContext<Chapter> flowContext) {
@@ -251,6 +183,116 @@ public interface FlowSupport {
         @ToString.Include
         private List<HistoryItem> allowedItems;
 
+    }
+
+    default List<Message> fulfillHistory(HistoryContext historyContext) {
+        var items = List.of(historyContext.getAllowedItems().isEmpty() ? HistoryItem.values() : historyContext.getAllowedItems().toArray(HistoryItem[]::new));
+        var names = new HashSet<ObjectName>();
+        var history = new ArrayList<Message>();
+        var previousContext = new StringBuilder();
+        history.add(new SystemMessage(historyContext.getSystemMessage()));
+        var systemTokens = countTokens(historyContext.getSystemMessage());
+        historyContext.getBudget().allocate(systemTokens);
+
+        if (items.contains(HistoryItem.CONSOLIDATED_CONTEXT)) {
+            historyContext.getFlowContext().hasAnyArgument(oldContext -> {
+                processContext(historyContext, oldContext, previousContext, history, USER_PREVIOUS_CONTEXT_TEMPLATE);
+            }, FlowContext::consolidatedContextArg, FlowContext::oldContexArg);
+        }
+
+        if (items.contains(HistoryItem.CONTEXT)) {
+            historyContext.getFlowContext().hasArgument(FlowContext::contextArg, context -> {
+                var currentContext = context.stringValue();
+                if (!previousContext.toString().equals(currentContext)) {
+                    processContext(historyContext, context, previousContext, history, USER_CONTEXT_TEMPLATE);
+                }
+            });
+        }
+
+        if (items.contains(HistoryItem.CONSOLIDATED_GLOSSARY)) {
+            historyContext.getFlowContext().hasAnyArgument(oldGlossary -> {
+                var uniqueGlossary = oldGlossary.map(list -> uniqueBy(list, ObjectName::getName));
+                processGlossary(historyContext, oldGlossary, uniqueGlossary, history, names, USER_PREVIOUS_GLOSSARY_TEMPLATE);
+            }, FlowContext::consolidatedGlossaryArg, FlowContext::oldGlossaryArg);
+        }
+
+        if (items.contains(HistoryItem.GLOSSARY)) {
+            historyContext.getFlowContext().hasArgument(FlowContext::glossaryArg, glossary -> {
+                var uniqueGlossary = glossary.map(chapterNames -> uniqueItems(names, chapterNames, ObjectName::getName));
+                processGlossary(historyContext, glossary, uniqueGlossary, history, names, USER_GLOSSARY_TEMPLATE);
+            });
+        }
+
+        return history;
+    }
+
+    private void processGlossary(HistoryContext historyContext, FlowArgument<List<ObjectName>> glossary,
+                                 FlowArgument<List<ObjectName>> uniqueGlossary, List<Message> history,
+                                 Set<ObjectName> names, String templateText) {
+        if (uniqueGlossary.isEmpty()) {
+            //TODO add logs here
+            return;
+        }
+
+        int itemTokens = glossary.countTokens();
+
+        boolean canAllocate = false;
+        String glossaryText;
+
+        if (historyContext.getBudget().canAllocate(itemTokens)) {
+            glossaryText = uniqueGlossary.stringValue();
+            canAllocate = true;
+        } else {
+            var shortStringValue = uniqueGlossary.shortStringValue();
+            int shortItemTokens = countTokens(shortStringValue);
+
+            if (historyContext.getBudget().canAllocate(shortItemTokens)) {
+                glossaryText = shortStringValue;
+                canAllocate = true;
+                itemTokens = shortItemTokens;
+            } else {
+                boolean hasArgument = historyContext.getFlowContext().hasArgument(alt(FlowContext::glossaryArg));
+                if (hasArgument) {
+                    var glossaryArg = historyContext.getFlowContext().resolve(alt(FlowContext::glossaryArg));
+                    Integer currentChapTokens = glossaryArg.countTokens();
+                    if (historyContext.getBudget().canAllocate(currentChapTokens)) {
+                        glossaryText = glossaryArg.stringValue();
+                        canAllocate = true;
+                        itemTokens = currentChapTokens;
+                    } else {
+                        var shortCurrentChapValue = glossaryArg.shortStringValue();
+                        int shortCurrentChapTokens = countTokens(shortStringValue);
+
+                        if (historyContext.getBudget().canAllocate(shortCurrentChapTokens)) {
+                            glossaryText = shortCurrentChapValue;
+                            canAllocate = true;
+                            itemTokens = shortCurrentChapTokens;
+                        } else {
+                            var smallArg = uniqueGlossary.map(val -> cut(val, subList -> glossary(subList).countTokens(), historyContext.getBudget()::canAllocate));
+                            glossaryText = smallArg.stringValue();
+                            canAllocate = true;
+                            itemTokens = smallArg.countTokens();
+                        }
+                    }
+                } else {
+                    var smallArg = uniqueGlossary.map(val -> cut(val, subList -> glossary(subList).countTokens(), historyContext.getBudget()::canAllocate));
+                    glossaryText = smallArg.stringValue();
+                    canAllocate = true;
+                    itemTokens = smallArg.countTokens();
+                }
+            }
+        }
+
+        if (canAllocate) {
+            history.add(new UserMessage(templateText));
+            history.add(new AssistantMessage(glossaryText));
+
+            names.addAll(glossary.getValue());
+
+            historyContext.getBudget().allocate(itemTokens);
+        } else {
+            //TODO add logs here
+        }
     }
 
 }
